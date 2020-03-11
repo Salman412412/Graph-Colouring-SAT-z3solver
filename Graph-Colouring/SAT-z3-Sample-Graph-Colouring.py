afae9bae3321f2bbf4c8a6f63ad706a7ad9b938c
Salman Masoumi <salman412412@gmail.com>
2020-03-11 08:30:56 +0330

    Update SAT-z3-Sample-Graph-Colouring.py
    A new boolean method

--- Graph-Colouring/SAT-z3-Sample-Graph-Colouring.py
+++ Graph-Colouring/SAT-z3-Sample-Graph-Colouring.py
@@ -16,24 +16,28 @@ import numpy as np
 with open('SampleGraph.txt', 'r') as f:
     lines = f.read().splitlines()
 	
-node_number = 0;
+node_number = 0
+edge_number = 0
 for line in lines:
+	edge_number = edge_number + 1
 	if int(line.split()[1]) > node_number:
 		node_number = int(line.split()[1])
 print('Number of nodes: ' + str(node_number))
+print('Number of edges: ' + str(edge_number))
 print('\n')
-
 
 # Ijade graph array
 Garray = np.zeros((node_number,node_number),dtype=bool)
 GarrayNo = np.zeros((node_number,1),dtype=int)
-Gcolor = [Int('Node%d_color' % (c+1)) for c in range(node_number)]
 
 for line in lines:
 	first_node = int(line.split()[0])
 	second_node = int(line.split()[1])
 	Garray[first_node-1, second_node-1] = True
 	GarrayNo[first_node-1,0] = GarrayNo[first_node-1,0] + 1
+	Garray[second_node-1, first_node-1] = True
+	GarrayNo[second_node-1,0] = GarrayNo[second_node-1,0] + 1
+
 
 # Morattabsazi Array nesbat be te'dade yalha
 for i in range(node_number):
@@ -41,33 +45,54 @@ for i in range(node_number):
 		if GarrayNo[j,0] > GarrayNo[i,0]:
 			GarrayNo[i,0], GarrayNo[j,0] = GarrayNo[j,0], GarrayNo[i,0]
 			Garray[[i,j]] = Garray[[j,i]]
+			Garray[:,[i, j]] = Garray[:,[j, i]]
 
+
+
+# Halghe baraye peyda kardane hadde aghalle range
+lower_band = 1
+upper_band = node_number - 1
 start = datetime.now()
-for ColorNo in range(node_number):
+
+while (upper_band > (lower_band+1)):
+	ColorNo = (upper_band+lower_band)//2
+	# Ta'rife moteghayyerha
+	Gcolor =[[Bool('Node_no%d_c%d' % (r+1,c+1)) for c in range(ColorNo)] for r in range(node_number)]
 	# Ta'rife solver
 	s = Solver()
-	print('Testing ' + str(ColorNo+1)+ ' Colour(s)')
-	
-	#Sharte Te'dade Rangha
-	for i in range(node_number):
-		s.add(Gcolor[i]>=1)
-		s.add(Gcolor[i]<(ColorNo+1))
+	print('Testing ' + str(ColorNo)+ ' Colour(s)')
 	
 	#Sharte yeksan nabudane range yalhaye mojaver
 	for i in range(node_number):
-		for j in range(node_number):
+		for j in range(i, node_number):
 			if Garray[i,j]:
-				s.add(Gcolor[i] != Gcolor[j])
-	
-	#Check kardane emkane vojude javab
-	if s.check() == sat:
-		print('Satisfied')
-		break
-	else:
-		print('Not Satisfied')
-		print('Time till now: '+ str(datetime.now()-start))
-		print('\n')
+				for k in range(ColorNo):
+					s.add(And(Gcolor[i][k],Gcolor[j][k]) == False)
+					
+	#Sharte dashtane yek rang baraye har node_number
+	for i in range(node_number):
+		for k in range(ColorNo):
+			for h in range(k,ColorNo):
+				if k != h:
+					s.add(And(Gcolor[i][k],Gcolor[i][h]) == False)
+					
+	#Sharte rang dashtane har node
+	for i in range(node_number):
+		s.add(Or([Gcolor[i][c] for c in range(ColorNo)]) == True)
 
+	#Check kardane emkane vojude javab
+	if s.check() == unsat:
+		print('Not Satisfied')
+		lower_band = ColorNo
+	else:
+		print('Satisfied')
+		upper_band = ColorNo
+	print('Time till now: '+ str(datetime.now()-start))
+	print('\n')
+
+		
 #Namayeshe Natayej
+print('\n')
 print('The calculation time is: ' + str(datetime.now()-start))
 print('The minimum number of Colours: ' + str(ColorNo))
+print('\n')
